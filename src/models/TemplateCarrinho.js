838c8db7b24c00b001383267e3455def87ff5616
Artur Comoli <artur.comoli@gmail.com>
2022-01-25 11:19:28 -0300

    classe estatica para criacao dos produtos no carrinho

---
+++ src/models/TemplateCarrinho.js
@@ -0,0 +1,28 @@
+class ProdutoCarrinho {
+    
+    static container = document.getElementById("vitrine-carrinho");
+
+    static
+        gerarProdutoCarrinho(produto) {
+        
+        const li = document.createElement("li")
+
+        this.container.innerHTML = "";
+
+        li.innerHTML =
+            `
+            <div><img src="${imagem}" alt=""></div>
+            <div>
+                <h2>${nome}</h2>
+                <p>${descricao}</p>
+                <span>R&#36;${preco}</span>
+            </div>
+            <div>
+                <button id="remover"><i class="fas fa-trash"></i></button>
+            </div>
+            `
+        this.container.appendChild(li)
+    }
+}
+
+export {ProdutoCarrinho}
